f560e566b95136fa28fd8f58ddd9e9dcfc5dfe69
hin101 <hineshpatel0@googlemail.com>
2019-04-22 09:42:02 +1000

    add currency cli

--- lib/curex.rb
+++ lib/curex.rb
@@ -1,5 +1,11 @@
 require "curex/version"
 require "hanami/cli"
+require "bigdecimal"
+require "money"
+require "money/bank/google_currency"
+
+Money.locale_backend = :currency
+Money.default_bank = Money::Bank::GoogleCurrency.new
 
 module Curex
   # Your code goes here...
@@ -13,8 +19,15 @@ module Curex
     extend Hanami::CLI::Registry
 
     class Convert < Hanami::CLI::Command
-      def call(*)
-        puts "converting.."
+      argument :amount, required: true
+      argument :from, required: true
+      argument :to, required: true
+
+      def call(amount:, from:, to:)
+        money = Money.new(amount.to_d * 100, from)
+        result = money.exchange_to(to)
+
+        puts "#{amount} #{from} == #{result} #{to}"
       end
     end
 
